fab5be249875c39a99da4e88f20ead2aa2ca8e24
Jeff Riffle <jeffriffle@gmail.com>
2026-05-29 16:11:49 -0500

    Add kubeconfig update to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+ansible-playbook -i inventory/k3s-ansible/hosts.ini site.yml "$@"
 
-ansible-playbook site.yml
+echo "==> Updating kubeconfig..."
+cp ~/k3s/kubeconfig ~/.kube/config
+chmod 600 ~/.kube/config
+sed -i 's|https://127.0.0.1:6443|https://192.168.1.216:6443|' ~/.kube/config
+
+echo "==> Cluster status:"
+kubectl get nodes
